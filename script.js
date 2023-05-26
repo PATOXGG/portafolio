2aa39c0b3e2b211e2c0a718611694aba2ab92b1e
PATOXGG <125672341+PATOXGG@users.noreply.github.com>
2023-05-25 18:58:43 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -38,5 +38,65 @@ function sign(){
         alert("es positivo");
     }
 }
+function salude(){
+    var num1, num2;
+    parseInt(num1);
+    parseInt(num2);
+    var nom=document.getElementById("nombre").value;
+    document.getElementById("resultado").innerHTML=nom;
+}
 
 
+function longitud(){
+   var nom= document.getElementById("nombre").value;
+    var long=nom.length;
+    document.getElementById("r").value=long;
+}
+
+function conc(){
+    var nom, ap;
+    nom=document.getElementById("nombre").value;
+    apellido=document.getElementById("ap").value;
+    nom=nom+" ";
+   var conc=nom.concat(apellido);
+   document.getElementById("r").value=conc;
+}
+
+function mayus(){
+    var nom, ap;
+    nom=document.getElementById("nombre").value;
+    apellido=document.getElementById("ap").value;
+    nom=nom+" ";
+   var conc=nom.concat(apellido);
+    var mayusc=conc.toUpperCase();
+    document.getElementById("r").value=mayusc;
+}
+
+function min(){
+    var nom=document.getElementById("nombre").value;
+    var ape=document.getElementById("ap").value;
+    nom=nom+" ";
+    var conc=nom.concat(ape);
+    var minus=conc.toLowerCase();
+    document.getElementById("r").value=minus;
+}
+function obchar(){
+    var nom=document.getElementById("nombre").value;
+    var indice=document.getElementById("n").value;
+    var res=nom.CharAt(indice);
+    document.getElementById("r").value=res;
+}
+function substring(){
+    var nom=document.getElementById("nombre").value;
+    
+}
+function split(){
+    
+    var nom=document.getElementById("nombre").value;
+    var ape=document.getElementById("ap").value;
+    nom=nom+" ";
+    var conc=nom.concat(ape);
+
+    var res=conc.split("");
+    document.getElementById("r").value=res;
+}
